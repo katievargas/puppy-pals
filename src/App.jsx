7b908c7a37cfd439139dda542d2deb3ca98491c7
Katie Vargas <you@example.com>
2024-01-20 20:05:05 -0500

    Update App component and data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import React, {useState} from 'react';
+import {puppyList} from './data.js'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+console.log(puppyList);
+
 function App() {
+  const [puppies,setPuppies] = useState(puppyList);
+
   return (
     <>
       <div>
@@ -13,9 +19,26 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Puppy Pals</h1>
+      <div>
+        <h2>List of Puppies</h2>
+        <ul>
+          {puppies.map((puppy) => (
+            <li key={puppy.id}>{puppy.name}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h2>Name of Puppies</h2>
+        <ul>
+          {puppies.map((puppy) => (
+            <li key={puppy.name}>{puppy.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
